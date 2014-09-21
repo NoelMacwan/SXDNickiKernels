b758058f83b60593730d266adb5ff7261d11dd1e
NoelMacwan <yulespencer@gmail.com>
2014-09-21 14:00:45 +0200

    pm8921-charger: implementing fast charge

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -114,6 +114,10 @@ do {\
 
 #define ABS(X) ((X) < 0 ? (-1 * (X)) : (X))
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define CHG_BUCK_CLOCK_CTRL	0x14
 #define CHG_BUCK_CLOCK_CTRL_8038	0xD
 
@@ -2398,7 +2402,39 @@ static void __pm8921_charger_vbus_draw(unsigned int mA)
 			i--;
 		if (i < 0)
 			i = 0;
+			
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge == 1)
+			i = 4;
+		else if (force_fast_charge == 2) {
+			switch (fast_charge_level) {
+				case FAST_CHARGE_500:
+					i = 1;
+					break;
+				case FAST_CHARGE_700:
+					i = 2;
+					break;
+				case FAST_CHARGE_900:
+					i = 4;
+					break;
+				case FAST_CHARGE_1100:
+					i = 5;
+					break;
+				case FAST_CHARGE_1300:
+					i = 6;
+					break;
+				case FAST_CHARGE_1500:
+					i = 7;
+					break;
+				default:
+					break;
+			}
+		}
 		rc = pm_chg_iusbmax_set(the_chip, i);
+		pr_info("charge curent index => %d\n", i);
+#else
+		rc = pm_chg_iusbmax_set(the_chip, i);       
+#endif
 		if (rc)
 			pr_err("unable to set iusb to %d rc = %d\n", i, rc);
 	}
